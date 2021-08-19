2e2b10336b32454f3ded9729a9941edbee4699d2
Vinayak Kulkarni <inbox.vinayak@gmail.com>
2021-08-19 18:03:26 +0530

    fix: lint issue

--- src/install.ts
+++ src/install.ts
@@ -1,33 +1,28 @@
-import { VueConstructor as Application, PluginFunction } from 'vue';
 import VueCompositionApi from '@vue/composition-api';
+import { PluginFunction, VueConstructor as Application } from 'vue';
+import CanvasLayer from './components/layer/CanvasLayer';
+import GeojsonLayer from './components/layer/GeojsonLayer';
+import ImageLayer from './components/layer/ImageLayer';
+import layerMixin from './components/layer/layerMixin';
+import RasterLayer from './components/layer/RasterLayer';
+import VectorLayer from './components/layer/VectorLayer';
+import VideoLayer from './components/layer/VideoLayer';
 import GlMap from './components/map/GlMap.vue';
-
-import NavigationControl from './components/UI/controls/NavigationControl';
-import GeolocateControl from './components/UI/controls/GeolocateControl';
-import FullscreenControl from './components/UI/controls/FullscreenControl';
 import AttributionControl from './components/UI/controls/AttributionControl';
+import controlMixin from './components/UI/controls/controlMixin';
+import FullscreenControl from './components/UI/controls/FullscreenControl';
+import GeolocateControl from './components/UI/controls/GeolocateControl';
+import NavigationControl from './components/UI/controls/NavigationControl';
 import ScaleControl from './components/UI/controls/ScaleControl';
 import Marker from './components/UI/Marker.vue';
 import Popup from './components/UI/Popup.vue';
-
-import GeojsonLayer from './components/layer/GeojsonLayer';
-import ImageLayer from './components/layer/ImageLayer';
-import CanvasLayer from './components/layer/CanvasLayer';
-import VideoLayer from './components/layer/VideoLayer';
-import VectorLayer from './components/layer/VectorLayer';
-import RasterLayer from './components/layer/RasterLayer';
-
-import withEventsMixin from './lib/withEvents';
 import withSelfEventsMixin from './components/UI/withSelfEvents';
-import controlMixin from './components/UI/controls/controlMixin';
-import layerMixin from './components/layer/layerMixin';
-
+import withEventsMixin from './lib/withEvents';
 import { setVueInstance } from './utils/config';
 
 let installed: boolean = false;
 
 const install: PluginFunction<Application> = (app: Application) => {
-  if (installed) return;
   if (!installed) {
     setVueInstance(app);
     const $helpers = {
