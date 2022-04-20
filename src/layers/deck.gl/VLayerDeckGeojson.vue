bfe18f019f4373a3d1c34500d16bf4ab90fd79d2
Vinayak Kulkarni <19776877+vinayakkulkarni@users.noreply.github.com>
2022-04-20 16:49:27 +0530

    feat(deck.gl): add `deck.gl` GeoJSON layer
    Signed-off-by: Vinayak Kulkarni <19776877+vinayakkulkarni@users.noreply.github.com>

---
+++ src/layers/deck.gl/VLayerDeckGeojson.vue
@@ -0,0 +1,80 @@
+<script lang="ts">
+  // @ts-ignore
+  import { GeoJsonLayer } from '@deck.gl/layers';
+  // @ts-ignore
+  import { MapboxLayer } from '@deck.gl/mapbox';
+  import { FeatureCollection } from 'geojson';
+  import type { PropType, Ref } from 'vue';
+  import { defineComponent, onMounted, ref, watch } from 'vue';
+  import { injectStrict, MapKey } from '../../utils';
+
+  export default defineComponent({
+    name: 'VLayerDeckGeojson',
+    props: {
+      layerId: {
+        type: String as PropType<string>,
+        default: 'deck.gl-geojson-layer',
+        required: true,
+      },
+      data: {
+        type: Object as PropType<FeatureCollection>,
+        required: true,
+      },
+      options: {
+        type: Object,
+        required: true,
+      },
+      before: {
+        type: String as PropType<string>,
+        default: '',
+        required: false,
+      },
+    },
+    setup(props) {
+      let map = injectStrict(MapKey);
+      let loaded: Ref<boolean> = ref(false);
+
+      const layer = new MapboxLayer({
+        ...props.options,
+        id: props.layerId,
+        data: props.data,
+        type: GeoJsonLayer,
+      });
+
+      map.value.on('style.load', () => {
+        // https://github.com/mapbox/mapbox-gl-js/issues/2268#issuecomment-401979967
+        const styleTimeout = () => {
+          if (!map.value.isStyleLoaded()) {
+            loaded.value = false;
+            setTimeout(styleTimeout, 200);
+          } else {
+            loaded.value = true;
+          }
+        };
+        styleTimeout();
+      });
+
+      /**
+       * Watcher(s)
+       */
+      watch(loaded, (value) => {
+        if (value) {
+          addLayer();
+        }
+      });
+
+      onMounted(() => {
+        addLayer();
+      });
+
+      /**
+       * Re–adds the layer when style changed
+       *
+       * @returns {void}
+       */
+      function addLayer(): void {
+        map.value.addLayer(layer, props.before);
+      }
+    },
+  });
+</script>
