ae11681cf6c05e0582961653c8509f59d371ae13
Vinayak Kulkarni <19776877+vinayakkulkarni@users.noreply.github.com>
2023-03-14 13:03:52 +0530

    chore: delete unnecessary shims 🧹
    Signed-off-by: Vinayak Kulkarni <19776877+vinayakkulkarni@users.noreply.github.com>

--- shims/@rollup/plugin-beep.d.ts
+++
@@ -1,5 +0,0 @@
-// TODO: Remove once https://github.com/rollup/plugins/pull/969 is merged
-declare module '@rollup/plugin-beep' {
-  import { Plugin } from 'rollup';
-  export default function beep(): Plugin;
-}
